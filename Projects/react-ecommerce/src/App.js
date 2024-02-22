871c82e83858eb298e151acc13d004ce6fbc0b28
Sachin7397 <sachinskamble0910@gmail.com>
2024-02-22 17:36:11 +0530

    changes

--- Projects/react-ecommerce/src/App.js
+++ Projects/react-ecommerce/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import ProducList from './features/product-list/ProductList';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-    <ProducList/>
+    <Home/>
     </div>
   );
 }
